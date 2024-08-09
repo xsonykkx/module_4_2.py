b1ede654f3205bfcd3fc11e9b669c144bf5d66c5
xsonykkx <sofiya.strologo@gmail.com>
2024-08-09 19:56:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def test_function():
-    inner_function()
     def inner_function():
-        def second_test():
-            print("Я в области видимости функции test_function")
+        inner_function()
+        print("Я в области видимости функции test_function")
 
-inner_function()
+test_function()
